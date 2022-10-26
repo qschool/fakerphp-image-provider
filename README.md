464dba0f833ed077622f10a312f8b98ef5a5c342
Mikhail Volkov <m.volkov@qsoft.ru>
2022-10-26 16:40:33 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# QSchool FakerPhp Image Provider
+
+[![Latest Version on Packagist](https://img.shields.io/packagist/v/qschool/fakerphp-image-provider.svg?style=flat-square)](https://packagist.org/packages/qschool/fakerphp-image-provider)
+[![Total Downloads](https://img.shields.io/packagist/dt/qschool/fakerphp-image-provider.svg?style=flat-square)](https://packagist.org/packages/qschool/fakerphp-image-provider)
+
+## Описание
+
+Провайдер для получения фейковых для [fakerphp](https://github.com/fakerphp/faker) изображения с [lorem.space](https://lorem.space)
+ 
+## Установка
+
+```bash
+composer require --dev qschool/fakerphp-image-provider
+```
+
+## Использование
+
+```php
+$faker = \Faker\Factory::create();
+$faker->addProvider(new \QSchool\FakerImageProvider\FakerImageProvider($faker));
+
+// Для получения строки url 'https://api.lorem.space/image/car?w=800&h=600'
+$faker->imageUrl($width = 800, $height = 600, $category = 'car'); 
+
+// Для скачивания изображения во временную директорию получения строки url
+$filename = $faker->image($dir = '/tmp', $width = 640, $height = 480, $category = 'car');
+```
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
---
+++ src/FakerImageProvider.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace QSchool\FakerImageProvider;
+
+use Faker\Provider\Base as BaseProvider;
+use InvalidArgumentException;
+use RuntimeException;
+
+class FakerImageProvider extends BaseProvider
+{
+    protected static string $baseUrl = "https://api.lorem.space/image/";
+
+    private const CATEGORIES = [
+        'album',
+        'book',
+        'car',
+        'face',
+        'fashion',
+        'furniture',
+        'game',
+        'movie',
+        'shoes',
+        'watch',
+    ];
+
+    public static function imageUrl(
+        int $width = 640,
+        int $height = 480,
+        ?string $category = null,
+    ): string {
+        if (empty($category) || ! in_array($category, self::CATEGORIES)) {
+            $category = self::CATEGORIES[array_rand(self::CATEGORIES)];
+        }
+
+        return  self::$baseUrl . $category . "?w={$width}&h={$height}";
+    }
+
+    public static function image(
+        string $dir = null,
+        int $width = 640,
+        int $height = 480,
+        ?string $category = null,
+    ): bool|RuntimeException|string {
+        return self::fetchImage(static::imageUrl($width, $height, $category), $dir);
+    }
+
+    private static function fetchImage(
+        string $url,
+        ?string $dir,
+    ): bool|RuntimeException|string {
+        $dir = $dir === null ? sys_get_temp_dir() : $dir;
+
+        if (! is_dir($dir) || ! is_writable($dir)) {
+            throw new InvalidArgumentException(sprintf('Cannot write to directory "%s"', $dir));
+        }
+
+        $name = md5(uniqid(empty($_SERVER['SERVER_ADDR']) ? '' : $_SERVER['SERVER_ADDR'], true));
+        $filename = $name . '.jpeg';
+        $filepath = $dir . DIRECTORY_SEPARATOR . $filename;
+
+        if (! function_exists('curl_exec')) {
+            return new RuntimeException('The image formatter downloads an image from a remote HTTP server. Therefore, it requires that PHP can request remote hosts, either via cURL or fopen()');
+        }
+
+        $fp = fopen($filepath, 'w');
+
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_FILE, $fp);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        $success = curl_exec($ch) && curl_getinfo($ch, CURLINFO_HTTP_CODE) === 200;
+        fclose($fp);
+        curl_close($ch);
+
+        if (! $success) {
+            unlink($filepath);
+
+            return false;
+        }
+
+        return $filepath;
+    }
+}
